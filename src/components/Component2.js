19f3e3b57efb84ebd3a202b95c159cfe51220b33
Ben Kissi <ben@benkissi.com>
2021-02-27 15:40:42 +0000

    refactor(): refactor components

--- src/components/Component2.js
+++ src/components/Component2.js
@@ -2,7 +2,7 @@ const Component2 = ({ isDarkMode }) => {
   return (
     <div
       style={{
-        background: isDarkMode ? "#000" : "#fff",
+        background: isDarkMode ? "#181818" : "#F8F8F8",
         width: "200px",
         height: "200px",
         border: "1px solid green",
